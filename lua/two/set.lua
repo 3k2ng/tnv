ca9d6482f23dc5250b4e6f79b30ed26306bd27d4
two <tung09102002@gmail.com>
2024-08-14 05:53:30 -0600

    trim comments

--- lua/two/set.lua
+++ lua/two/set.lua
@@ -1,11 +1,9 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
-
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.autoindent = true
-
 vim.opt.ignorecase = true
-
 vim.opt.cursorline = true
+vim.wo.signcolumn = "yes:2"
