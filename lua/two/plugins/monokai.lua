11bdee031cd33d8e96afbdd4796668cec54d077e
3k2ng <tung09102002@gmail.com>
2026-01-22 21:10:16 +0700

    what do you mean that the theme is gone???

--- lua/two/plugins/monokai.lua
+++ lua/two/plugins/monokai.lua
@@ -2,6 +2,6 @@ return {
     "loctvl842/monokai-pro.nvim",
     config = function()
         require("monokai-pro").setup()
-        vim.cmd("colorscheme monokai-pro-machine")
+        vim.cmd("colorscheme monokai-pro")
     end,
 }
